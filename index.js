f37932e711df2c33675b7c6d2c00c4213fd2851c
Geeta Chinmayee Paruchuri <128633256+chinmayeeparuchuri@users.noreply.github.com>
2025-03-10 17:45:19 +0530

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; 
-import App from "./App";
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 import { AuthProvider } from "./context/AuthContext"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -9,4 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </AuthProvider>
 );
 
-
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
